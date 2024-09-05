701eeacae6df9064cd2d7a7d2824b756a59f19ac
SebaUni <94626126+SebaUni@users.noreply.github.com>
2024-09-05 16:48:42 -0400

    Initial Content
    Definido el Contenido Inicial que tendra la react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRoute as Router, Routes, Route} from 'react-router-dom';
+import homePage from './Components/Pages/Home/homePage';
+import productPage from './Components/Pages/Product/productPage';
+import teamPage from './Components/Pages/Team/teamPage';
+import contactPage from './Components/Pages/Contact/contactPage';
+import Navbar from './Components/Navbar';
+import Footbar from './Components/Footbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<homePage/>}/>
+            <Route path="/Homepage" element={<homePage/>}/>
+            <Route path="/Product" element={<productPage/>}/>
+            <Route path="/Team" element={<teamPage/>}/>
+            <Route path="/Contancts" element={<contactPage/>}/>
+          </Routes>
+          <Footbar/>
+        </div>
+      </Router>
     </div>
   );
 }
-
-export default App;

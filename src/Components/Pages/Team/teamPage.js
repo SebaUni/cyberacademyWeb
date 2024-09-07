0ab4ad30005d2017a58a74e2e999117a4fa7c977
SebaUni <94626126+SebaUni@users.noreply.github.com>
2024-09-06 23:11:38 -0400

    Update Website Teampage
    Actualizado los cambios realizados sobre la pagina de equipo

--- src/Components/Pages/Team/teamPage.js
+++ src/Components/Pages/Team/teamPage.js
@@ -2,12 +2,83 @@
 //import { useState } from 'react';
 import "./teamStyle.css";
 
+const Logos = require("./../../../Assets/logos");
 
 //Vista asociada a la Seccion del Equipo
 const TeamPage = () =>{
     return (
         <div className="Content">
-            <h1>Team</h1>
+            <div className="Team">
+                <h1>Equipo de trabajo</h1>
+                <div className="Members">
+                    <div className="Member">
+                        <div className="Photo">
+                            <Logos.LogoApp />
+                        </div>
+                        <div className="Info">
+                            <b>Nombre:</b> Nombre_Integrante_1
+                        </div>
+                        <div className="Info">
+                            <b>Rol:</b> Rol_Integrante_1
+                        </div>
+                    </div>
+                    <div className="Member">
+                        <div className="Photo">
+                            <Logos.LogoApp />
+                        </div>
+                        <div className="Info">
+                            <b>Nombre:</b> Nombre_Integrante_2
+                        </div>
+                        <div className="Info">
+                            <b>Rol:</b> Rol_Integrante_2
+                        </div>
+                    </div>
+                    <div className="Member">
+                        <div className="Photo">
+                            <Logos.LogoApp />
+                        </div>
+                        <div className="Info">
+                            <b>Nombre:</b> Nombre_Integrante_3
+                        </div>
+                        <div className="Info">
+                            <b>Rol:</b> Rol_Integrante_3
+                        </div>
+                    </div>
+                    <div className="Member">
+                        <div className="Photo">
+                            <Logos.LogoApp />
+                        </div>
+                        <div className="Info">
+                            <b>Nombre:</b> Nombre_Integrante_4
+                        </div>
+                        <div className="Info">
+                            <b>Rol:</b> Rol_Integrante_4
+                        </div>
+                    </div>
+                    <div className="Member">
+                        <div className="Photo">
+                            <Logos.LogoApp />
+                        </div>
+                        <div className="Info">
+                            <b>Nombre:</b> Nombre_Integrante_5
+                        </div>
+                        <div className="Info">
+                            <b>Rol:</b> Rol_Integrante_5
+                        </div>
+                    </div>
+                    <div className="Member">
+                        <div className="Photo">
+                            <Logos.LogoApp />
+                        </div>
+                        <div className="Info">
+                            <b>Nombre:</b> Nombre_Integrante_6
+                        </div>
+                        <div className="Info">
+                            <b>Rol:</b> Rol_Integrante_6
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
